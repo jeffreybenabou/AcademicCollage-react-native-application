5f38dcd4c15dbd08667deb1e9eb16a86bcdee980
hagar <hagar.ifrah94@gmail.com>
2020-12-29 14:33:50 +0200

    ...

---
+++ ios/NotificationsImages/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationsImages
+//
+//  Created by Elector on 29/12/2020.
+//
+
+#import <UserNotifications/UserNotifications.h>
+#import "FirebaseMessaging.h"
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
